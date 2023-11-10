61aa2384a05fb1a7f53d80539c353842a863877d
Viktoria Zechner <v_zechner@gmx.at>
2023-11-10 10:58:52 +0100

    exercise about loops

---
+++ rosalind/exercise4Loops.py
@@ -0,0 +1,18 @@
+a = 4656
+b = 9652
+result = 0
+
+#for n in range(0, b-a+1):
+    #print("iteration", a, "adding new number", a+n, "to result", result)
+    #result = result + (a + n)
+#print(result)
+
+
+#second option
+result = 0
+for n in range(a, b+1):
+    #is n odd? if yes->
+    if n % 2 == 1: #% is the modular (Restfunktion)
+        result= result + n
+ 
+print(result)
