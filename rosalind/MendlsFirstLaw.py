cd6dccf58f45e3184e46ee475a931ce332a8edfe
Viktoria Zechner <v_zechner@gmx.at>
2023-11-24 10:39:06 +0100

    solved Mendels first law

--- rosalind/MendlsFirstLaw.py
+++ rosalind/MendlsFirstLaw.py
@@ -7,9 +7,25 @@
 # probability of any offspring being Ax (having one dominant Allel) -> Pr(offspring) = Ax 
 # output is a decimal number between 0 and 1 probability)
 
+# all formulas for every possible pair of parents + the probability to have pr(Ax)
+# first parent k
+#kk = k/(k+m+n) * k-1/(k+m+n-1) * 1
+#km = k/(k+m+n) * m/(k+m+n-1) * 1
+#kn = k/(k+m+n) * n/(k+m+n-1) * 1
+#first parent m
+#mm = m/(k+m+n) * m-1/(k+m+n-1) * 0.75
+#mn = m/(k+m+n) * n/(k+m+n-1) * 0.5
+#mk = m/(k+m+n) * k/(k+m+n-1) * 1
+#first parent n
+#nn = n/(k+m+n) * n-1/(k+m+n-1) * 0
+#nk = n/(k+m+n) * k/(k+m+n-1) * 1
+#nm = n/(k+m+n) * m/(k+m+n-1) * 0.5
 
-# k/(k+m+n) * k/(k+m+n-1) * 1
-# m/(k+m+n) * m/(k+m+n-1) * 0.75
-# n/(k+m+n) * n/(k+m+n-1) * 0
-# 
+# number of individuals
+k = 16
+m = 28
+n = 26
 
+# calculate the probability
+function = k/(k+m+n) * (k-1)/(k+m+n-1) * 1 +k/(k+m+n) * m/(k+m+n-1) * 1 + k/(k+m+n) * n/(k+m+n-1) * 1 + m/(k+m+n) * (m-1)/(k+m+n-1) * 0.75 + m/(k+m+n) * n/(k+m+n-1) * 0.5 + m/(k+m+n) * k/(k+m+n-1) * 1 + n/(k+m+n) * (n-1)/(k+m+n-1) * 0 + n/(k+m+n) * k/(k+m+n-1) * 1 + n/(k+m+n) * m/(k+m+n-1) * 0.5
+print(function)
